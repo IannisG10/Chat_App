ad3b350accb3d1081f5d1a05f935ce6202bca958
IannisG10 <guerraiannis@gmail.com>
2025-02-19 13:39:48 +0100

    feat/state management: configuration de redux et du store pour la gestion des etats

---
+++ src/redux/slice/productSlice.tsx
@@ -0,0 +1,11 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const productSlice = createSlice({
+    name: 'product',
+    initialState: {}
+    reducers:{
+        
+    }
+})

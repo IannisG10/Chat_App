6347da7b6a49892b5f70ed3a1a2bb5e018e303b6
IannisG10 <guerraiannis@gmail.com>
2025-04-17 09:42:29 +0200

    form: toogle the visibility og password field with `toggleInputType` function

--- src/components/block/Input/InputAuth.tsx
+++ src/components/block/Input/InputAuth.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react"
+import React, { useState } from "react"
 import { Input } from "@/components/ui/Input"
 import { InputProps } from "@/entities/compoent-entities/input-entities"
 import { InputField } from "@/entities/application-entities/authentication"
@@ -7,8 +7,14 @@ import {useFormContext} from "react-hook-form"
 import { Eye,EyeClosed } from "lucide-react"
 
 export const InputAuth = ({placeholder,label,options,type} : InputProps) => {
+
     const {register,formState: {errors}} = useFormContext<InputField>()
     const isPassword = type === "password" && (label === "password" || label === "confirm_password")
+    const [isVisiblePassword,setIsVisiblePassword] = useState<boolean>(false)
+
+    const toggleInputType = () => {
+        setIsVisiblePassword(!isVisiblePassword)
+    }
     return(
         <> 
             <div className=" relative">
@@ -18,7 +24,22 @@ export const InputAuth = ({placeholder,label,options,type} : InputProps) => {
                         {...register(label,options)}
                         type={type}
                 />
-                {isPassword && <EyeClosed size={19} className=" absolute top-2.5 right-1.5 cursor-pointer"/>}
+                {isPassword && 
+                    isVisiblePassword ? 
+                        <Eye className=" absolute top-2.5 right-1.5 cursor-pointer" size={19} onClick={toggleInputType}/> : 
+                        <EyeClosed className=" absolute top-2.5 right-1.5 cursor-pointer" size={19} onClick={toggleInputType}/>
+                    // <EyeClosed className=" absolute top-2.5 right-1.5 cursor-pointer" size={19}/>
+                    // isVisiblePassword ? 
+                        // <EyeClosed size={19} 
+                        //     className=" absolute top-2.5 right-1.5 cursor-pointer"
+                        //     onClick={toggleInputType}
+                        // /> : 
+                        // <Eye size={19}
+                        //     className=" absolute top-2.5 right-1.5 cursor-pointer"
+                        //     onClick={toggleInputType}
+                        // />
+
+                }
             </div>
            <div className=" flex justify-center font-medium">
                 {errors[label] && <span className=" text-sm  text-red-600">{errors[label]?.message}</span>}

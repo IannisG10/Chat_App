68a7afbf0aa2595308954ad3b51193b092e4d3e1
Iannis GUERRA <144343272+IannisG10@users.noreply.github.com>
2025-04-27 18:52:20 +0300

    Update NavBar.tsx

--- src/components/block/NavBar/NavBar.tsx
+++ src/components/block/NavBar/NavBar.tsx
@@ -10,9 +10,18 @@ export const NavBar = ({firstItem,secondItem,thirdItem}: NavBarType) => {
     return(
         <nav className="flex items-center justify-around bg-[#2c2c2c] md:w-1/4 w-11/12  rounded-3xl">
             <ul className=" flex text-white justify-between md:w-3/4 w-full  ">
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >{firstItem}</li>
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >{secondItem}</li>
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >{thirdItem}</li>
+                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
+                    <Link href="">
+                        {firstItem}
+                    </Link>
+                    
+                </li>
+                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
+                    {secondItem}
+                </li>
+                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
+                    {thirdItem}
+                </li>
             </ul>
             <div className="md:block hidden rounded-full p-1 hover:bg-[#535353] transition-all duration-150 cursor-pointer">
                 <BiSolidMoon color="white" size={25}/>

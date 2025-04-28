64a392d0a01ee167ff390219810d2131437842c8
IannisG10 <guerraiannis@gmail.com>
2025-04-28 14:37:16 +0200

    active link navbar

--- src/components/block/NavBar/NavBar.tsx
+++ src/components/block/NavBar/NavBar.tsx
@@ -11,16 +11,15 @@ export const NavBar = ({firstItem,secondItem,thirdItem}: NavBarType) => {
     return(
         <nav className="flex items-center justify-around bg-[#2c2c2c] md:w-1/4 w-11/12  rounded-3xl">
             <ul className=" flex text-white justify-between md:w-3/4 w-full  ">
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
+                <Link href="" className={`cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2 ${path === "/" && "bg-[#535353]"}`} >
                         {firstItem}
-                </li>
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
-                   
+                </Link>
+                <Link href="" className={`cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2 ${path === ""}`} >
                         {secondItem}
-                </li>
-                <li className=" cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2" >
+                </Link>
+                <Link href="" className={`cursor-pointer hover:bg-[#535353] transition-all duration-150 md:py-2 py-1.5 md:text-base text-sm rounded-3xl px-2 ${path === ""}`} >
                         {thirdItem}
-                </li>
+                </Link>
             </ul>
             <div className="md:block hidden rounded-full p-1 hover:bg-[#535353] transition-all duration-150 cursor-pointer">
                 <BiSolidMoon color="white" size={25}/>

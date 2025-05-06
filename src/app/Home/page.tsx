4da136e86ed150cd0c63ddd77f78c2ceb1546bb6
IannisG10 <guerraiannis@gmail.com>
2025-05-06 19:15:34 +0200

    func: add a asynchronous function to get user

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -7,8 +7,19 @@ import { FeedBack } from "@/components/sections/FeedBack/FeedBack";
 import { Footer } from "@/components/sections/Footer/Footer";
 import { IconRouteType } from "@/components/sections/Header/Header.types";
 import {BiBell,BiLogOutCircle,BiMessageRounded} from "react-icons/bi"
+import axios from "axios";
 
-export default function Acceuil(){
+const getUser = async () => {
+    const res = await axios.get("https://chat-app-api-5pvs.onrender.com/auth",{
+        withCredentials: true
+    })
+    return res.data
+
+}
+
+export default async function Acceuil(){
+
+    
     const icoList: IconRouteType[] = [
         {
             href: "",
